4ca7d0db55f75e9b24875542d53e6021bae55f1d
white <chenqing0718@163.com>
2021-02-26 18:45:13 +0800

    保存

--- netease_cloud_music/lib/pages/video_detail/video_detail.dart
+++ netease_cloud_music/lib/pages/video_detail/video_detail.dart
@@ -7,6 +7,7 @@ class VideoDetail extends StatefulWidget{
 }
 
 class _VideoDetailState extends State<VideoDetail>{
+  PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -17,59 +18,64 @@ class _VideoDetailState extends State<VideoDetail>{
           '视频详情'
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 120),
-            child: Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  width: ScreenUtil.screenWidth,
-                  height: 200,
-                  color: Colors.red,
-                ),
-                Container(
-                  width: 40,
-                  height: 40,
-                  color: Colors.blue,
-                ),
-                Positioned(
-                    right: 15,
-                    child: Container(
-                      child: Icon(
-                        Icons.phone_android_rounded,
-                        size: 25,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.grey
-                      ),
-                    )
-                )
-              ],
-            ),
-          ),
-          Container(
-            child: Column(
-              children: [
-                Container(
-                  child: Image.asset(
-                    'images/icon_event_video_b_play.png',
-                    width: 25,
-                    height: 30,
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    '内容'
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: PageView.builder(
+          itemBuilder: (context,index){
+            return Container();
+          }
+      )
+      // Column(
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   children: [
+      //     Container(
+      //       margin: EdgeInsets.only(top: 120),
+      //       child: Stack(
+      //         alignment: AlignmentDirectional.center,
+      //         children: [
+      //           Container(
+      //             width: ScreenUtil.screenWidth,
+      //             height: 200,
+      //             color: Colors.red,
+      //           ),
+      //           Container(
+      //             width: 40,
+      //             height: 40,
+      //             color: Colors.blue,
+      //           ),
+      //           Positioned(
+      //               right: 15,
+      //               child: Container(
+      //                 child: Icon(
+      //                   Icons.phone_android_rounded,
+      //                   size: 25,
+      //                 ),
+      //                 decoration: BoxDecoration(
+      //                   color: Colors.grey
+      //                 ),
+      //               )
+      //           )
+      //         ],
+      //       ),
+      //     ),
+      //     Container(
+      //       child: Column(
+      //         children: [
+      //           Container(
+      //             child: Image.asset(
+      //               'images/icon_event_video_b_play.png',
+      //               width: 25,
+      //               height: 30,
+      //             ),
+      //           ),
+      //           Container(
+      //             child: Text(
+      //               '内容'
+      //             ),
+      //           )
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
